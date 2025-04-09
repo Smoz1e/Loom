c60464b54f19c7f1513fc4eefc86154d4c84bfe9
Ilya <ib258650@gmail.com>
2025-04-09 16:54:32 +0500

    Editing front page Login and page Register

--- Loom/Loom/urls.py
+++ Loom/Loom/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from RegAndAuth.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Register),
+    path('login/', Login)
 ]
--- Loom/RegAndAuth/views.py
+++ Loom/RegAndAuth/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+def Register(request):
+    return render(request, "register.html")
+
+def Login(request):
+    return render(request, "login.html")
+
 # Create your views here.
