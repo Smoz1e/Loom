eb643d4e8931a3325c496510764b1af91145d345
Ilya <ib258650@gmail.com>
2025-05-21 19:02:47 +0500

    made display thems v 0.7

--- Loom/HomePageUser/templatetags/calendar_extras.py
+++ Loom/HomePageUser/templatetags/calendar_extras.py
@@ -9,3 +9,24 @@ def get_item(dictionary, key):
 def until(value, end):
     """Возвращает range(value, end) для шаблонных циклов."""
     return range(value, end)
+
+@register.filter
+def category_to_color_class(category):
+    mapping = {
+        'Работа/Учёба': 'blue-task',
+        'Бытовые дела': 'purple-task',
+        'Время, проведённое с семьёй': 'green-task',
+        'Время, проведённое с друзьями': 'orange-task',
+        'Личные дела': 'red-task',
+        'Праздники': 'yellow-task',
+        'Тег: Важно': 'priority-high',
+        'Тег: Обычное': 'priority-normal',
+        'Тег: Низкий приоритет': 'priority-low',
+        'Синий': 'blue-task',
+        'Фиолетовый': 'purple-task',
+        'Зелёный': 'green-task',
+        'Оранжевый': 'orange-task',
+        'Красный': 'red-task',
+        'Жёлтый': 'yellow-task',
+    }
+    return mapping.get(category, 'blue-task')
